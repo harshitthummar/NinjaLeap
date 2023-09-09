99b13bc3efadee004d6cd74edebb49d3f5dbc248
Harshit Thummar <itsharshitthumar@gmail.com>
2023-09-09 22:59:25 +0530

    optimize camera control

--- Scripts/Core/CameraController.cs
+++ Scripts/Core/CameraController.cs
@@ -8,10 +8,15 @@ public class CameraController : MonoBehaviour
 
     [Header("Camera Config")]
     [SerializeField] private float AheadDistance;
+    [SerializeField] private float CameraSpeed;
 
+    private float LookAhead;
     // Update is called once per frame
     private void Update()
     {
-        transform.position = new Vector3(playercam.position.x /* * AheadDistance*/,playercam.position.y,transform.position.z);
+       // transform.position = new Vector3(playercam.position.x /* * AheadDistance*/,playercam.position.y,transform.position.z);
+    
+        transform.position = new Vector3(playercam.position.x + LookAhead, transform.position.y,transform.position.z);
+        LookAhead = Mathf.Lerp(LookAhead, (AheadDistance * playercam.localScale.x),Time.deltaTime * CameraSpeed);
     }
 }
