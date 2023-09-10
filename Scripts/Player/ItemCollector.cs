8aa53d5fa03bb2db826d773869863e76222181ce
Harshit Thummar <itsharshitthumar@gmail.com>
2023-09-10 17:24:31 +0530

    added score mechanism

--- Scripts/Player/ItemCollector.cs
+++ Scripts/Player/ItemCollector.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class ItemCollector : MonoBehaviour
 {
     private int Score=0;
+
+    [SerializeField] private Text ScoreText; 
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("strawberry"))
         {
             Destroy(collision.gameObject);
             Score++;
-            Debug.Log("Score - " + Score);
+            ScoreText.text = "Score: " + Score;
         }
     }
 }
