f30c9060de3066ddcbe7ffd07cca4729345ab372
Harshit Thummar <itsharshitthumar@gmail.com>
2023-09-13 09:12:42 +0530

    added leveling system

--- Scripts/Finish.cs
+++ Scripts/Finish.cs
@@ -1,26 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
 
     private AudioSource FinishSound;
+
+    private bool islevelcompleted=false;
     private void Start()
     {
         FinishSound = GetComponent<AudioSource>();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && !islevelcompleted)
         {
             FinishSound.Play();
-            CompleteLevel();
+            islevelcompleted = true;
+            Invoke("CompleteLevel", 3f);
+           
         }
         
     }
     private void CompleteLevel()
     {
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
